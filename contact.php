3dcc70a11deab897be2c906538202d4f1a245dd5
TomJSmith <thomas_smith@live.ca>
2016-06-12 23:01:34 -0600

    changed all .html files to .php in preperation of php implementataion. Updated the contact page to have html forms. Changed a bit on the home page.

---
+++ contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thomas Smith</title>
+<link rel="stylesheet" href="css/mainPage.css">
+</head>
+<body>
+<div class="mainFrame">
+  <div class="menuBar">
+  <ul>
+    <li><a href="mainPage.php">Home</a></li>
+    <li><a href="photography.php">Photography</a></li>
+    <li><a href="projects.php">Projects</a></li>
+    <li><a href="contact.php" style="background-color: #0F5959">Contact</a></li>
+  </ul>
+  </div>
+  <div class="contentFrame">
+    <div class="contactFormFrame">
+      <form class="contactForm" action="TODO" method="post">
+        <h3>Name<br></h3>
+        <input type="text" name="nameInputBox" value="">
+        <h3>Email</h3>
+        <input type="email" name="emailInputBox" value="">
+        <h3>Message</h3>
+        <textarea name="message" rows="8" cols="40"></textarea>
+        <button type="button" name="button">Send</button>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="socialLinks">
+  <a href="https://www.instagram.com/thom_john/"><img src="instagram.png" class="socialIcon" height="30px" width="30px"/></a>
+  <a href="https://www.facebook.com/profile.php?id=1081272861"><img src="facebook-black.png" class="socialIcon" height="30px" width="30px"/></a>
+  <a href="https://github.com/TomJSmith"><img src="github.png" class="socialIcon" height="30px" width="30px"/></a>
+  <a href="https://ca.linkedin.com/in/thomas-smith-39686289"><img src="linkedin.png" class="socialIcon" height="30px" width="38px"/></a>
+</div>
+</body>
+</html>
---
+++ mainPage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Thomas Smith</title>
+<link rel="stylesheet" href="css/mainPage.css">
+</head>
+<body>
+<div class="mainFrame">
+  <div class="menuBar">
+    <ul>
+      <li><a href="mainPage.php" style="background-color: #0F5959">Home</a></li>
+      <li><a href="photography.php">Photography</a></li>
+      <li><a href="projects.php">Projects</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </div>
+  <div class="contentFrame">
+    <div class="heading">
+      <h1>Thomas Smith</h1>
+    </div>
+    <div class="left">
+      <div class="resume">
+        <div class="resumeItem">
+          <h4>Computer Science, University of Calgary</h4>
+          <h5>Software Design, Algorithms, AI, Game Design, 3D Graphics, HCI</h5>
+        </div>
+        <div class="resumeItem">
+            <h4>Data Analyst, Precision Drilling Corporation</h4>
+            <h5>Python, VBA</h5>
+        </div>
+      </div>
+    </div>
+    <div class="right">
+      <img src="scubaProfile.jpg" class="circleImage" height="200px" width="200px"/>
+    </div>
+  </div>
+</div>
+<div class="socialLinks">
+  <a href="https://www.instagram.com/thom_john/"><img src="instagram.png" class="socialIcon" height="30px" width="30px"/></a>
+  <a href="https://www.facebook.com/profile.php?id=1081272861"><img src="facebook-black.png" class="socialIcon" height="30px" width="30px"/></a>
+  <a href="https://github.com/TomJSmith"><img src="github.png" class="socialIcon" height="30px" width="30px"/></a>
+  <a href="https://ca.linkedin.com/in/thomas-smith-39686289"><img src="linkedin.png" class="socialIcon" height="30px" width="38px"/></a>
+</div>
+</body>
+</html>
